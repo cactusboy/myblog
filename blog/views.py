59233e0e7ac033de879589199b2ddfe885b5a407
cactusboy <13159098@qq.com>
2018-05-07 15:57:26 +0800

    # 180507 修改模板

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import  HttpResponse
+from .models import Post
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("欢迎访问我的博客首页")
+    post_list = Post.objects.all().order_by('-created_time')
+    # 显示所有文章，order_by是排序，-created_time是按创建时间倒序排列
+    return render(request, 'blog/index.html', context={
+        'title': '我的博客首页',
+        'welcome': '欢迎访问我的博客首页',
+        'post_list': post_list
+    })
