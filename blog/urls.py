76a44a55546510dd2f51a3a61cb6922611dffdcb
cactysboy@qq.com <cactysboyqq.com@cactysboyqqcomdeMacBook-Air.local>
2018-05-08 23:26:59 +0800

    markdown

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,6 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path(r'', views.index, name='index'),
-    path(r'post/', views.detail, name='detail'),
+    path(r'post/(?P<pk>[0-9] +)/$', views.detail, name='detail'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
+import markdown
 from django.shortcuts import get_object_or_404, render
 # from django.http import  HttpResponse
 from .models import Post
+
+
 # Create your views here.
 
 
@@ -8,12 +11,18 @@ def index(request):
     post_list = Post.objects.all().order_by('-created_time')
     # 显示所有文章，order_by是排序，-created_time是按创建时间倒序排列
     return render(request, 'blog/index.html', context={
-        'title': '我的博客首页',
-        'welcome': '欢迎访问我的博客首页',
+        # 'title': '我的博客首页',
+        # 'welcome': '欢迎访问我的博客首页',
         'post_list': post_list
     })
 
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/detail.html', context={'post':post})
+    # 记得引入markdown模块
+    post.body = markdown.markdown(post.body, extensions=[
+        'markdown.extensions.extra',
+        'markdown.extensions.codehilite',
+        'markdown.extensions.toc',
+    ])
+    return render(request, 'blog/detail.html', context={'post': post})
