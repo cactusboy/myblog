1a2a28982df5d0d855ec9d29fd200adc8c7a02c3
cactusboy <13159098@qq.com>
2018-05-09 15:56:13 +0800

    关于表单的进一步解释

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 import markdown
 from django.shortcuts import get_object_or_404, render
 # from django.http import  HttpResponse
-from .models import Post
+from .models import Post, Category
 
 
 # Create your views here.
@@ -26,3 +26,17 @@ def detail(request, pk):
         'markdown.extensions.toc',
     ])
     return render(request, 'blog/detail.html', context={'post': post})
+
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month,
+                                    ).order_by("-created_time")
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    # 记得开始部分导入Category类
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
--- blogproject/settings.py
+++ blogproject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # 我的应用
     'blog',
+    'comments',
 ]
 
 MIDDLEWARE = [
